cb4a807ec7c4e105eaa62a34794f6a7a53e6cd93
김초원 <zzzcc33@naver.com>
2022-03-05 14:18:06 +0900

    자바 8 API의 기본 메소드와 스태틱 메소드

---
+++ src/com/chocho/study/chapter02/chapter02_2/Main.java
@@ -0,0 +1,54 @@
+package com.chocho.study.chapter02.chapter02_2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+
+/**
+ * 자바8 API에 추가된 기본 메소드와 스태틱 메소드
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> name = new ArrayList<>();
+        name.add("초원");
+        name.add("성학");
+        name.add("수진");
+        name.add("민수");
+
+        System.out.println("<<---Iterable 인터페이스의 기본 메소드--->");
+        System.out.println("<<---1.forEach()--->");
+        name.forEach(System.out::println);
+
+        System.out.println("<<---2.spliterator()--->");
+        Spliterator<String> spliterator1 = name.spliterator();
+        Spliterator<String> spliterator2 = spliterator1.trySplit();
+
+        while (spliterator1.tryAdvance(System.out::println));
+        System.out.println("=========");
+        while (spliterator2.tryAdvance(System.out::println));
+
+        System.out.println("\n<<---Collection 인터페이스의 기본 메소드--->");
+        System.out.println("<<---1.stream()--->");
+        List<String> k = name.stream().map(String::toUpperCase)
+                .filter(s -> s.startsWith("K"))
+                .collect(Collectors.toList());
+
+        System.out.println("<<---2.removeIf(Predicate)--->");
+        name.removeIf(s -> s.startsWith("K"));
+
+        System.out.println("<<--- --->");
+
+        System.out.println("\n<<---Comparator 인터페이스의 기본 메소드--->");
+        System.out.println("<<---1.reversed()--->");
+        Comparator<String> comparator = String::compareToIgnoreCase;
+        name.sort(comparator.reversed());
+
+        System.out.println("<<---2.thenComparing--->");
+        Comparator<String> comparator2 = Comparator.comparingInt(String::length);
+        name.sort(comparator.reversed().thenComparing(comparator2));
+    }
+}
