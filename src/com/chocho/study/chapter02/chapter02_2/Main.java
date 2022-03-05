50d8d87dd2dad6437e900a331cf33ff2604f2448
김초원 <zzzcc33@naver.com>
2022-03-05 14:25:03 +0900

    자바 8 API의 기본 메소드와 스태틱 메소드

--- src/com/chocho/study/chapter02/chapter02_2/Main.java
+++ src/com/chocho/study/chapter02/chapter02_2/Main.java
@@ -19,11 +19,11 @@ public class Main {
         name.add("수진");
         name.add("민수");
 
-        System.out.println("<<---Iterable 인터페이스의 기본 메소드--->");
-        System.out.println("<<---1.forEach()--->");
+        System.out.println("Iterable 인터페이스의 기본 메소드");
+        System.out.println("1.forEach()");
         name.forEach(System.out::println);
 
-        System.out.println("<<---2.spliterator()--->");
+        System.out.println("2.spliterator()");
         Spliterator<String> spliterator1 = name.spliterator();
         Spliterator<String> spliterator2 = spliterator1.trySplit();
 
@@ -31,23 +31,21 @@ public class Main {
         System.out.println("=========");
         while (spliterator2.tryAdvance(System.out::println));
 
-        System.out.println("\n<<---Collection 인터페이스의 기본 메소드--->");
-        System.out.println("<<---1.stream()--->");
+        System.out.println("\nCollection 인터페이스의 기본 메소드");
+        System.out.println("1.stream()");
         List<String> k = name.stream().map(String::toUpperCase)
                 .filter(s -> s.startsWith("K"))
                 .collect(Collectors.toList());
 
-        System.out.println("<<---2.removeIf(Predicate)--->");
+        System.out.println("2.removeIf(Predicate)");
         name.removeIf(s -> s.startsWith("K"));
-
-        System.out.println("<<--- --->");
-
-        System.out.println("\n<<---Comparator 인터페이스의 기본 메소드--->");
-        System.out.println("<<---1.reversed()--->");
+        
+        System.out.println("\nComparator 인터페이스의 기본 메소드");
+        System.out.println("1.reversed()");
         Comparator<String> comparator = String::compareToIgnoreCase;
         name.sort(comparator.reversed());
 
-        System.out.println("<<---2.thenComparing--->");
+        System.out.println("2.thenComparing");
         Comparator<String> comparator2 = Comparator.comparingInt(String::length);
         name.sort(comparator.reversed().thenComparing(comparator2));
     }
